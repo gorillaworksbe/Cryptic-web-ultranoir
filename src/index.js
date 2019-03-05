ea5d21fc6a7c96a6ae8ef24be4b2bbdb03828a15
Daniel Velasquez <anemoleiros@gmail.com>
2019-03-05 01:34:11 -0500

    Initial threeJS setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import "./styles.css";
+import * as THREE from "three";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+function GLManager() {
+  this.camera = new THREE.PerspectiveCamera(45, 1, 0.1, 10000);
+  this.camera.position.z = 5;
+
+  this.scene = new THREE.Scene();
+  this.camera.lookAt = this.scene.position;
+
+  const geo = new THREE.BoxGeometry(1, 1, 1);
+  const mat = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const mesh = new THREE.Mesh(geo, mat);
+
+  this.scene.add(mesh);
+
+  this.renderer = new THREE.WebGLRenderer({ antialias: true });
+  this.renderer.setSize(window.innerWidth, window.innerHeight);
+  this.renderer.setPixelRatio(window.devicePixelRatio);
+  document.getElementById("app").appendChild(this.renderer.domElement);
+}
+GLManager.prototype.render = function() {
+  this.renderer.render(this.scene, this.camera);
+};
+
+const GL = new GLManager();
+GL.render();
